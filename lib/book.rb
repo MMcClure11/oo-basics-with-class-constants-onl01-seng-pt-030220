430aaf4804bb424b7751db3848f8657131e435f3
Megan Christine McClure <mcclme02@luther.edu>
2020-03-31 00:52:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -21,8 +21,8 @@
 
 
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
  
   GENRES = []
  
@@ -32,6 +32,11 @@ class Book
  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
   
 end
