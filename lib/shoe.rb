b0452d22f53cf5821dd96a0aa5769b79d51a54c6
Megan Christine McClure <mcclme02@luther.edu>
2020-03-31 00:44:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,19 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
 
-  BRANDS = [ ]
+#   BRANDS = [ ]
  
-  def initialize(brand)
-    @brand = brand
-    BRANDS << brand
-    BRANDS.uniq!
-  end
+#   def initialize(brand)
+#     @brand = brand
+#     BRANDS << brand
+#     BRANDS.uniq!
+#   end
  
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
 
-end
+# end
